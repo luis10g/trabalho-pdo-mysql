b82e9b35795367076b1f8e568647d60b8f4703a7
luis10g <joicefernandasouzaoliveira@gmail.com>
2021-11-14 17:10:48 +0000

    banco de dados

---
+++ assets/sql/dados_iniciais.sql
@@ -0,0 +1,11 @@
+insert into generos (id, nome) values 
+(1, 'Fantasia'),
+(2, 'Policial'),
+(3, 'Drama'),
+(4, 'Ficção Científica');
+
+insert into livros (id, titulo, id_genero) values
+    (1, 'Assassinato no expresso do Oriente', 2),
+    (2, 'A sociedade do Anel', 1),
+    (3, 'Como era eu antes de você', 3),
+    (4, 'O guia do mochileiro das Galáxias', 4);
