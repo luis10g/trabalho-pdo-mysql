22726e8d797754c6e84679754e242c69605c280a
luis10g <joicefernandasouzaoliveira@gmail.com>
2021-11-14 17:56:32 +0000

    adicionando o bootstrap

--- index.php
+++ index.php
@@ -17,10 +17,13 @@ $generos = $comando->fetchAll(PDO::FETCH_ASSOC);
 <head>
     <meta charset="UTF-8">
     <title>BIBLIOTECA</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" 
+integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
 </head>
 <body>
-    <a href="insert.php">Novo Gênero</a>
-        <table>
+    <main class="container">
+    <a class="btn btn-primary" href="insert.php">Novo Gênero</a>
+        <table class="table">
             <tr>
                 <th>ID</th>
                 <th>Nome</th>
@@ -31,11 +34,12 @@ $generos = $comando->fetchAll(PDO::FETCH_ASSOC);
                     <td><?= $g['id']?></td>
                     <td><?=$g['nome'] ?></td>
                     <td>
-                        <a href="update.php?id=<?= $g['id'] ?>">Editar</a>
-                        <a href="delete.php?id=<?= $g['id'] ?>">Excluir</a>
+                        <a class="btn btn-secondary" href="update.php?id=<?= $g['id'] ?>">Editar</a>
+                        <a class="btn btn-danger" href="delete.php?id=<?= $g['id'] ?>">Excluir</a>
                     </td>
                 </tr>
                 <?php endforeach ?>
         </table>
+        </main>
 </body>
 </html> 
--- update.php
+++ update.php
@@ -26,14 +26,23 @@ if($_SERVER['REQUEST_METHOD'] == 'GET') {
 <head>
     <meta charset="UTF-8">
     <title>Editar Gênero</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" 
+
+integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
 </head>
 <body>
+<main class="container">
     <h1>Editar Gênero</h1>
     <form action="update.php" method="post">
         <input type="hidden" name="id" value="<?= $genero['id'] ?>" />
+        <div class="form-group">
         <label for="nome">Nome do Gênero</label>
-        <input type="text" required name="nome" value="<?= $genero['nome'] ?>" />
-        <button type="submit">Salvar</button>
+        <input class="form-control" type="text" required name="nome" value="<?= $genero['nome'] ?>" />
+        </div>
+        <br>
+        <a class="btn btn-secondary" href="index.php">Voltar</a>
+        <button class="btn btn-success" type="submit">Salvar</button>
     </form>
+</main>
 </body>
 </html>
